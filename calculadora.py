b944ea8ff0eeafbb02ceee5a3ea0bdfad4b6523e
Vitória Medeiros Mendes <intoria2020@icloud.com>
2026-06-17 16:50:27 -0300

    3v: Minha Primeira Calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,5 @@
-while True:
+historico = [] #este comando permite que os dados registrados na calculadora sejam guardados
+while True:    #while permite que o usuário escolha fazer mais calculos ou não
     print ("Calculadora iniciada")
     num1 = int(input("digite o primeiro numero: ")) 
     num2 = int(input("digite o segundo numero: "))
@@ -17,8 +18,10 @@ while True:
         resultado = num1/num2
 
     print("O Resultado é: ", resultado)
+    historico.append (resultado)
 
     opcao = input("Deseja realizar outra operação? (S/N): ")
 
     if opcao == "N":
-        break
+        print ("Seu histórico de resultados é: ",historico)
+        break   #a condiçaõ "break" interrompe com um comando  arepetição do calculo
