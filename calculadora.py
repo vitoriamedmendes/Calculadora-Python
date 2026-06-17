a5bd553237a1d9c6db6e8db7321729d35a98adbf
Vitória Medeiros Mendes <intoria2020@icloud.com>
2026-06-17 15:15:13 -0300

    2v: Minha Primeira Calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,18 +1,24 @@
-print ("Calculadora iniciada")
-num1 = int(input("digite o primeiro numero: ")) 
-num2 = int(input("digite o segundo numero: "))
-operacao = input("Digite a operação (+, -, *, /): ")
+while True:
+    print ("Calculadora iniciada")
+    num1 = int(input("digite o primeiro numero: ")) 
+    num2 = int(input("digite o segundo numero: "))
+    operacao = input("Digite a operação (+, -, *, /): ")
 
-if operacao == "+": 
-    resultado = num1 + num2
+    if operacao == "+": 
+        resultado = num1 + num2
 
-elif operacao == "-": 
-    resultado = num1 - num2
+    elif operacao == "-": 
+        resultado = num1 - num2
 
-elif operacao == "*":
-    resultado = num1*num2
+    elif operacao == "*":
+        resultado = num1*num2
 
-elif operacao =="/":
-    resultado = num1/num2
+    elif operacao =="/":
+        resultado = num1/num2
 
-print("O Resultado é: ", resultado)
+    print("O Resultado é: ", resultado)
+
+    opcao = input("Deseja realizar outra operação? (S/N): ")
+
+    if opcao == "N":
+        break
